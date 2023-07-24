978bd4b5f43fbf1c8c6a227dd12a96798451b3d8
mahmoud <mahmoudwardany18@gmail.com>
2023-07-24 19:01:36 +0300

    use commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express=require('express')
 const cors=require('cors')
 const connectDB = require('./DB/config')
 const morgan = require('morgan')
-
+const compression =require("compression")
 require('dotenv').config()
 
 //db connect
@@ -17,7 +17,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-
+app.use(compression())
 //router
 app.use(`/api/v1/auth`,require('./router/authRoute'))
 app.use(`/api/v1/category`,require('./router/categoryRoute'))
