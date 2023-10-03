f9cb8f34d12425ac4fab45caa1725770f87e3bd7
mahmoud <mahmoudwardany18@gmail.com>
2023-10-03 19:35:33 +0300

    Edit

--- controller/auth/registerController.js
+++ controller/auth/registerController.js
@@ -1,29 +1,33 @@
-const { hashPassword } = require("../../helper/hashPassword")
-const { validRegister, userModel } = require("../../models/userModel")
+const { hashPassword } = require("../../helper/hashPassword");
+const { validRegister, userModel } = require("../../models/userModel");
+
 const registerController = async (req, res) => {
-  const { name, email, password, phone,address,answer } = req.body
+  const { name, email, password, phone, address, answer } = req.body;
 
-  const { error } = validRegister({ name, email, password, phone ,address,answer})
-  error && res.status(400).json({ message: error.details[0].message })
-  try {
-    const emailExsit = await userModel.findOne({ email })
-    if (emailExsit) {
-      return res.status(400).send({ message: "email already exsit", success: false })
-    }
-    const phoneExsit = await userModel.findOne({ phone })
-    if (phoneExsit) {
-      return res.status(400).json({ message: "phone already exsit", success: false  })
-    }
-    const hashedPassword = await hashPassword(password)
-    const newUser = new userModel({ name, email, password: hashedPassword, phone ,address,answer}).save()
-    res.status(201).json({ message: "Register successfully", success: true })
-  } catch (error) {
-    res.status(400).json({ message: "failed to register", success: false,error  })
-
+  const { error } = validRegister({ name, email, password, phone, address, answer });
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
   }
-}
 
+  try {
+    const emailExists = await userModel.findOne({ email });
+    if (emailExists) {
+      return res.status(400).json({ message: "Email already exists", success: false });
+    }
 
+    const phoneExists = await userModel.findOne({ phone });
+    if (phoneExists) {
+      return res.status(400).json({ message: "Phone already exists", success: false });
+    }
 
+    const hashedPassword = await hashPassword(password);
+    const newUser = new userModel({ name, email, password: hashedPassword, phone, address, answer });
+    await newUser.save();
 
-module.exports = registerController
+    return res.status(201).json({ message: "Registered successfully", success: true, newUser });
+  } catch (error) {
+    return res.status(400).json({ message: "Failed to register", success: false, error });
+  }
+};
+
+module.exports = registerController;
